4cf351bf7e27f8798486ee9a46ba111fc5ba8b38
Ravi Ranjan Kumar <raviranjan.kumar@tivo.com>
2017-12-25 14:28:55 +0530

    initial commit

---
+++ Lambdas/src/java8/exercises/lambda/Greeter.java
@@ -0,0 +1,15 @@
+package java8.exercises.lambda;
+
+public class Greeter {
+
+    public void greet(){
+        System.out.println("Hello World!");
+
+    }
+
+    public static void main(String[] args){
+        Greeter greeter = new Greeter();
+        greeter.greet();
+
+    }
+}
